dc702df4ea9b09b6e365af7e93c3b1fc17920d45
Rian Drake <rian.drake@rocketwerkz.com>
2024-03-14 09:58:32 +1300

    fix copilot

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -431,4 +431,4 @@ vim.opt.conceallevel = 2
 
 -- Copilot configuration
 vim.g.copilot_no_tab_map = true
-vim.api.vim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
