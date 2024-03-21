c540ee3a0006195249079340948f732a8adb9129
Rian Drake <rian.drake@rocketwerkz.com>
2024-03-21 19:17:16 +1300

    Implemented some abbreviations for time/date

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -3,3 +3,9 @@ vim.keymap.set("n", "<leader>w", ":w<CR>")
 vim.keymap.set("n", "<leader>W", ":wq<CR>")
 vim.keymap.set("n", "<leader>q", ":q<CR>")
 vim.keymap.set("n", "<leader>Q", ":qa<CR>")
+
+vim.cmd([[
+iab <expr> ddate strftime("%Y-%m-%d")
+iab <expr> ttime strftime("%H:%M")
+iab <expr> ptime strftime("%l:%M%p")
+]])
