b4d9841f1ddc17c8b6e4c58605ab30d9b2e30f13
Rian Drake <rian.drake@rocketwerkz.com>
2024-04-18 20:05:55 +1200

    adding neotree

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -263,6 +263,20 @@ require('lazy').setup({
   },
 
   {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    config = function()
+      vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal float<CR>')
+    end,
+  },
+
+  {
     'jackMort/ChatGPT.nvim',
     event = 'VeryLazy',
     config = function()
